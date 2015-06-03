c848f6a3e0f9d6b647a5b29ff46e1ba800870825
thammin <rim_paulyoung@cyberagent.co.jp>
2015-06-03 14:38:54 +0900

    fix(sanitize): replace RegExp.test with String.match
    this fix the old browsers that could not get the correct value if using
    reuse same RegExp object.

--- angular-sanitize.js
+++ angular-sanitize.js
@@ -307,7 +307,7 @@ function htmlParser(html, handler) {
           chars = false;
         }
       // DOCTYPE
-      } else if (DOCTYPE_REGEXP.test(html)) {
+      } else if (!!html.match(DOCTYPE_REGEXP)) {
         match = html.match(DOCTYPE_REGEXP);
 
         if (match) {
@@ -315,7 +315,7 @@ function htmlParser(html, handler) {
           chars = false;
         }
       // end tag
-      } else if (BEGING_END_TAGE_REGEXP.test(html)) {
+      } else if (!!html.match(BEGING_END_TAGE_REGEXP)) {
         match = html.match(END_TAG_REGEXP);
 
         if (match) {
@@ -325,7 +325,7 @@ function htmlParser(html, handler) {
         }
 
       // start tag
-      } else if (BEGIN_TAG_REGEXP.test(html)) {
+      } else if (!!html.match(BEGIN_TAG_REGEXP)) {
         match = html.match(START_TAG_REGEXP);
 
         if (match) {
